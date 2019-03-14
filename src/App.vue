41b67cae26d15a8196b57461a43ea7eb59dc744f
Lova <maitrejedi@lovasoa.fr>
2019-03-14 11:28:50 +0100

    feat(footer): change author name

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <div id="img-wrapper" :class="{infos: showInfos}" @click="menuIsOpen = !menuIsOpen">
-      <img 
-        :src="imgSrc"
-        alt="show me love">
+      <img :src="imgSrc" alt="show me love">
     </div>
     <div class="buttons" :class="{infos: showInfos}">
       <button class="btn" @click="prev">Prev</button>
@@ -15,12 +13,19 @@
         Choose a chapter:
         <label for="chapter"></label>
         <select v-model="currentChapter" id="chapter">
-          <option disabled value="">Choisissez</option>
-          <option v-for="chapter in $_chapters" :key="chapter.name" :value="chapter.name - 1">{{chapter.name}}</option>
+          <option disabled value>Choisissez</option>
+          <option
+            v-for="chapter in $_chapters"
+            :key="chapter.name"
+            :value="chapter.name - 1"
+          >{{chapter.name}}</option>
         </select>
       </div>
       <div class="menu-credit">
-        <small>Made with <a href="https://www.buymeacoffee.com/Lova" target="_blank" rel="noopener">🥜</a> by Side <a href="https://lovasoa.fr" target="_blank" rel="noopener">🐯</a></small>
+        <small>Made with
+          <a href="https://www.buymeacoffee.com/Lova" target="_blank" rel="noopener">🥜</a> by Lova
+          <a href="https://lovasoa.fr" target="_blank" rel="noopener">🐯</a>
+        </small>
       </div>
     </div>
     <div id="progress" ref="progress"></div>
@@ -28,82 +33,87 @@
 </template>
 
 <script>
-import { chapters } from '../public/hajime-no-ippo.json'
+import { chapters } from "../public/hajime-no-ippo.json";
 
 export default {
-  name: 'app',
+  name: "app",
   data: () => {
     return {
-      baseUrl: 'https://vf-scan.com/mangas/hajime-no-ippo',
+      baseUrl: "https://vf-scan.com/mangas/hajime-no-ippo",
       currentChapter: parseInt(localStorage.currentChapter) || 0,
       currentImg: parseInt(localStorage.currentImg) || 0,
       imgs: [],
       menuIsOpen: false,
       showInfos: false
-    }
+    };
   },
   created() {
-    this.$_chapters = chapters
-    this.loadChapter(this.$_chapters[this.currentChapter])
+    this.$_chapters = chapters;
+    this.loadChapter(this.$_chapters[this.currentChapter]);
   },
   mounted() {
-    this.$_body = document.getElementsByTagName('body')[0]
-    this.setProgress()
+    this.$_body = document.getElementsByTagName("body")[0];
+    this.setProgress();
     if (this.currentImg === 0 && this.currentChapter === 0) {
-      this.showInfos = true
-      setTimeout(() => { this.showInfos = false }, 5000)
+      this.showInfos = true;
+      setTimeout(() => {
+        this.showInfos = false;
+      }, 5000);
     }
   },
   computed: {
     imgSrc() {
-      if (!this.imgs.length || this.currentImg === -1) return ""
-      return this.imgs[this.currentImg].src
+      if (!this.imgs.length || this.currentImg === -1) return "";
+      return this.imgs[this.currentImg].src;
     }
   },
   methods: {
     chap() {
-      return this.$_chapters[this.currentChapter]
+      return this.$_chapters[this.currentChapter];
     },
     loadChapter(chapter) {
       this.imgs = chapter.pages.map(page => {
-        const img = new Image()
-        img.src = `${this.baseUrl}/${chapter.name}/${page}.jpg`
-        return img
+        const img = new Image();
+        img.src = `${this.baseUrl}/${chapter.name}/${page}.jpg`;
+        return img;
       });
     },
     prev() {
-      this.currentImg = this.currentImg <= 0 ? this.currentImg : this.currentImg - 1
+      this.currentImg =
+        this.currentImg <= 0 ? this.currentImg : this.currentImg - 1;
     },
     next() {
-      if (this.currentImg >= this.imgs.length - 1) return
-      this.currentImg++
+      if (this.currentImg >= this.imgs.length - 1) return;
+      this.currentImg++;
       if (this.imgs.length - 1 === this.currentImg) {
-        this.menuIsOpen = true
+        this.menuIsOpen = true;
       }
     },
     setProgress() {
-      this.$refs.progress.style.width = parseInt( (this.currentImg + 1) / this.imgs.length * 100) + "vw"
-      this.$_body.scrollBy(1080, 0)
+      this.$refs.progress.style.width =
+        parseInt(((this.currentImg + 1) / this.imgs.length) * 100) + "vw";
+      this.$_body.scrollBy(1080, 0);
     }
   },
-  watch:{
+  watch: {
     currentChapter() {
-      this.currentImg = 0
-      this.loadChapter(this.$_chapters[this.currentChapter])
-      localStorage.currentChapter = this.currentChapter
-      this.menuIsOpen = false
-      this.$_body.scrollBy(1080, 0)
+      this.currentImg = 0;
+      this.loadChapter(this.$_chapters[this.currentChapter]);
+      localStorage.currentChapter = this.currentChapter;
+      this.menuIsOpen = false;
+      this.$_body.scrollBy(1080, 0);
     },
     currentImg() {
-      localStorage.currentImg = this.currentImg
-      this.setProgress()
+      localStorage.currentImg = this.currentImg;
+      this.setProgress();
     }
   }
-}
+};
 </script>
 
 <style>
-html, body {
+html,
+body {
   padding: 0;
   margin: 0;
   max-height: 100vh;
@@ -122,20 +132,20 @@ a {
 #app {
   height: 100vh;
   position: relative;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#app .buttons{
+#app .buttons {
   position: fixed;
   bottom: 0;
   height: 36vh;
   display: flex;
   width: 100vw;
 }
-.buttons button{
+.buttons button {
   opacity: 0;
   flex: 50%;
   transition: opacity 0.5s ease;
@@ -143,9 +153,9 @@ a {
   font-weight: 900;
 }
 .buttons.infos button {
-  opacity: 0.4
+  opacity: 0.4;
 }
-.buttons button:last-child{
+.buttons button:last-child {
   right: 0;
 }
 img {
@@ -159,7 +169,7 @@ img {
   position: fixed;
   top: 0;
   bottom: 36vh;
-  background-color: #BADA55;
+  background-color: #bada55;
   display: flex;
   justify-content: center;
   align-items: center;
